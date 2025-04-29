6264c822aea653841092da14ebfb53666253353f
electorn <daadiitya.007@gmail.com>
2025-04-30 05:14:58 +0530

    Data base setup and user register end point

---
+++ backend/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const userSchema=new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true ,
+        trim: true,
+        lowercase: true,
+        minLength: [6,'email must be at least 6'],
+        maxLength: [50,'email must be at least 50'],
+        
+
+    },
+  
+    password: {
+        type: String,
+        select: false,
+       
+    }
+
+})
+
+
+
+userSchema.statics.hashPassword=async function(password) {
+    return await bcrypt.hash(password,10);
+}
+
+userSchema.methods.isValidPassword=async function(password) {
+    return await bcrypt.compare(password,this.password);
+}
+userSchema.methods.generateJWT= function() {
+    return jwt.sign({email:this.email},process.env.JWT_SECRET);
+}
+
+const User=mongoose.model('user',userSchema);
+export default User;

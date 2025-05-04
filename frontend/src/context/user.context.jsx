4c95710295572a81d9e24afbc1715bef9ccdccf8
electorn <daadiitya.007@gmail.com>
2025-05-05 04:54:58 +0530

    socket io is working

--- frontend/src/context/user.context.jsx
+++ frontend/src/context/user.context.jsx
@@ -5,13 +5,11 @@ export const UserContext = createContext();
 
 // Create a provider component
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+    const [ user, setUser ] = useState(null);
 
-   
     return (
         <UserContext.Provider value={{ user, setUser }}>
             {children}
         </UserContext.Provider>
     );
 };
-
--- frontend/src/screens/Login.jsx
+++ frontend/src/screens/Login.jsx
@@ -1,13 +1,16 @@
-import React, { useState,useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from '../config/axios'
-import{UserContext} from '../context/user.context'
+import { UserContext } from '../context/user.context'
 
 const Login = () => {
 
+
     const [ email, setEmail ] = useState('')
     const [ password, setPassword ] = useState('')
+
     const { setUser } = useContext(UserContext)
+
     const navigate = useNavigate()
 
     function submitHandler(e) {
@@ -29,22 +32,22 @@ const Login = () => {
         })
     }
 
-
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-900">
             <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md">
                 <h2 className="text-2xl font-bold text-white mb-6">Login</h2>
                 <form
-                     onSubmit={submitHandler}
+                    onSubmit={submitHandler}
                 >
                     <div className="mb-4">
                         <label className="block text-gray-400 mb-2" htmlFor="email">Email</label>
                         <input
-                            onChange={(e) => setEmail(e.target.value)} 
+
+                            onChange={(e) => setEmail(e.target.value)}
                             type="email"
                             id="email"
                             className="w-full p-3 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            required
+                            placeholder="Enter your email"
                         />
                     </div>
                     <div className="mb-6">
@@ -54,7 +57,7 @@ const Login = () => {
                             type="password"
                             id="password"
                             className="w-full p-3 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            required
+                            placeholder="Enter your password"
                         />
                     </div>
                     <button
